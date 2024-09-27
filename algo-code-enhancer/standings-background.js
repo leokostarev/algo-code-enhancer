ec8f5af7b5510a21400e5dc7c798232c88672abc
BadCatSet <leokostarev@mail.ru>
2024-09-27 16:17:39 +0300

    chore: added support for firefox fix: better loading refactor: get rid of "potentially unsafe" code fix: support any number of info columns feat: sort by score option

--- algo-code-enhancer/standings-background.js
+++ algo-code-enhancer/standings-background.js
@@ -1,95 +1,138 @@
-let ALGO_STANDINGS_FIXED = null;
-let ALGO_STANDINGS = null;
+/** @type {HTMLTableElement} */
+let ALGO_STANDINGS_FIXED = null, ALGO_STANDINGS = null;
 
 (async function () {
     // ждем пока таблица загрузится
-    while (ALGO_STANDINGS === null || ALGO_STANDINGS.children.length === 0 ||
-    ALGO_STANDINGS_FIXED === null || ALGO_STANDINGS_FIXED.children.length === 0) {
-        await new Promise(resolve => setTimeout(resolve, 500));
+    while (true) {
         ALGO_STANDINGS_FIXED = document.getElementById("standings_fixed");
         ALGO_STANDINGS = document.getElementById("standings");
-        console.log("aaa");
-    }
-    // получаем и настраиваем кнопку
-    let penalty = ALGO_STANDINGS_FIXED.children[0].children[0].children[4];
-    penalty.style.color = "blue";
-    penalty.style.fontWeight = "bold";
 
-    {
-        // добавляем новый ряд с количеством сданных задач и настраиваем таблицу
-        // для боковой панели
-        let table_body = ALGO_STANDINGS_FIXED.children[0];
-        /** @type {HTMLTableRowElement} */
-        let head_row = table_body.children[0];
-        let space_row = table_body.children[1];
+        if (ALGO_STANDINGS === null ||
+            ALGO_STANDINGS.children.length === 0 ||
+            ALGO_STANDINGS_FIXED === null ||
+            ALGO_STANDINGS_FIXED.children.length === 0) {
+            await new Promise(resolve => setTimeout(resolve, 200));
+        } else {
+            break;
+        }
+    }
+
+    const num_of_info_rows = ALGO_STANDINGS_FIXED.tBodies[0].rows[0].cells.length - 1;
+
+    { // добавляем новый ряд с количеством сданных задач и настраиваем таблицу для боковой панели
+        const table_body = ALGO_STANDINGS_FIXED.tBodies[0];
+
+        const head_row = table_body.rows[0];
+        const space_row = table_body.rows[1];
+
         space_row.insertAdjacentElement("afterend", space_row.cloneNode(true));
 
-        for (let i = 0; i < 5; i++) {
-            head_row.children[i].rowSpan = 3;
+        for (let i = 0; i < num_of_info_rows; i++) {
+            head_row.cells[i].rowSpan = 3;
         }
     }
-    {
-        // добавляем новый ряд с количеством сданных задач и настраиваем таблицу
-        // для основной части
-        let table_body = ALGO_STANDINGS.children[1];
-        /** @type {HTMLTableRowElement} */
-        let head_row = table_body.children[0];
-        let task_row = table_body.children[1];
+
+    { // добавляем новый ряд с количеством сданных задач и настраиваем таблицу для основной части
+        const table_body = ALGO_STANDINGS.tBodies[0];
+
+        const head_row = table_body.rows[0];
+        const task_row = table_body.rows[1];
         task_row.insertAdjacentElement("afterend", task_row.cloneNode(true));
 
-        for (let i = 0; i < 5; i++) {
-            head_row.children[i].rowSpan = 3;
+        for (let i = 0; i < num_of_info_rows; i++) {
+            head_row.cells[i].rowSpan = 3;
         }
-        let count_row = table_body.children[2];
-        let task_count = count_row.children.length - 1;
-        // всё подсчитаем
-        let counter = Array(task_count).fill(0);
-        for (let ri = 3; ri < table_body.children.length - 1; ri++) {
-            let row = table_body.children[ri];
 
-            for (let ci = 0; ci < task_count; ci++) {
-                if (row.children[ci + 5].classList.contains("ok")) {
-                    counter[ci]++;
+        const count_row = table_body.rows[2];
+        const num_of_tasks = count_row.cells.length - 1;
+        // подсчитаем количество успешных решений
+        const counter = Array(num_of_tasks).fill(0);
+        for (let row_i = 3; row_i < table_body.rows.length - 1; row_i++) {
+            const row = table_body.rows[row_i];
+
+            for (let column_i = 0; column_i < num_of_tasks; column_i++) {
+                if (row.cells[column_i + num_of_info_rows].classList.contains("ok")) {
+                    counter[column_i]++;
                 }
             }
         }
 
         // и вводим данные в таблицу
-        for (let ci = 0; ci < task_count; ci++) {
-            count_row.children[ci].innerHTML = counter[ci];
-            count_row.children[ci].style.color = "green";
-            count_row.children[ci].style.fontWeight = "bold";
+        for (let column_i = 0; column_i < num_of_tasks; column_i++) {
+            count_row.cells[column_i].textContent = counter[column_i];
+            count_row.cells[column_i].style.color = "green";
+            count_row.cells[column_i].style.fontWeight = "bold";
         }
     }
 
-    // добавляю клик в конце, чтобы не нарушить работу моего кода
-    penalty.onclick = () => {
-        AlgoPenaltySort(ALGO_STANDINGS_FIXED.children[0]);
-        AlgoPenaltySort(ALGO_STANDINGS.children[1]);
-    };
+    { // получаем и настраиваем кнопку, добавляю реакцию на клик в конце, чтобы не нарушить работу моего кода
+        const score_button = ALGO_STANDINGS_FIXED.tBodies[0].rows[0].cells[3];
+        const penalty_button = ALGO_STANDINGS_FIXED.tBodies[0].rows[0].cells[4];
 
+        const default_color = score_button.style.color;
+        const default_weight = score_button.style.fontWeight;
+        const new_color = "blue";
+        const new_weight = "bold";
+
+        function sort_by_score(a, b) {
+            return (Number(b.cells[3].textContent) - Number(a.cells[3].textContent)) * 100000 +
+                (Number(a.cells[4].textContent) - Number(b.cells[4].textContent));
+        }
+
+        function sort_by_penalty(a, b) {
+            return (Number(b.cells[4].textContent) - Number(a.cells[4].textContent)) * 100000 +
+                (Number(b.cells[3].textContent) - Number(a.cells[3].textContent));
+        }
+
+        function activate_penalty_button() {
+            score_button.style.color = default_color;
+            score_button.style.fontWeight = default_weight;
+            penalty_button.style.color = new_color;
+            penalty_button.style.fontWeight = new_weight;
+        }
+
+        function activate_score_button() {
+            score_button.style.color = new_color;
+            score_button.style.fontWeight = new_weight;
+            penalty_button.style.color = default_color;
+            penalty_button.style.fontWeight = default_weight;
+        }
+
+        activate_penalty_button();
+
+        score_button.onclick = () => {
+            AlgoPenaltySort(ALGO_STANDINGS_FIXED.tBodies[0], sort_by_score);
+            AlgoPenaltySort(ALGO_STANDINGS.tBodies[0], sort_by_score);
+            activate_penalty_button();
+        };
+        penalty_button.onclick = () => {
+            AlgoPenaltySort(ALGO_STANDINGS_FIXED.tBodies[0], sort_by_penalty);
+            AlgoPenaltySort(ALGO_STANDINGS.tBodies[0], sort_by_penalty);
+            activate_score_button();
+        };
+    }
 })();
 
-
-function AlgoPenaltySort(table_body) {
+/** @param {HTMLTableSectionElement} table_body
+ * @param { (a: HTMLTableRowElement, b: HTMLTableRowElement) => number} compare_function */
+function AlgoPenaltySort(table_body, compare_function) {
     // выбираем все строки с данными
     /** @type {HTMLTableRowElement[]} */
-    let data_rows = [];
-    for (let i = 3; i < table_body.children.length; i++) {
-        data_rows.push(table_body.children[i]);
+    const data_rows = [];
+    for (let i = 3; i < table_body.rows.length; i++) {
+        data_rows.push(table_body.rows[i]);
     }
+
     // удаляем их из таблицы
-    for (let row of data_rows) {
+    for (const row of data_rows) {
         table_body.removeChild(row);
     }
 
     // сортируем данные
-    data_rows.sort((a, b) =>
-        -Number(a.children[4].innerHTML) + Number(b.children[4].innerHTML),
-    );
+    data_rows.sort(compare_function);
 
     // добавляем их обратно в таблицу
-    for (let row of data_rows) {
+    for (const row of data_rows) {
         table_body.appendChild(row);
     }
 }
